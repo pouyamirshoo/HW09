b62d6186200b7c780eec3746c9bdbcd07f8d2dcd
pc <poyatajhiz59@gmail.com>
2024-01-27 14:05:24 -0800

    creat and fill BaseEntity class

---
+++ src/main/java/base/model/BaseEntity.java
@@ -0,0 +1,16 @@
+package base.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class BaseEntity<ID extends Serializable> {
+    private ID id;
+}
