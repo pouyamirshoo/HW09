aa31771c8a0c3d0a450db5649029e6b5c2170913
pc <poyatajhiz59@gmail.com>
2024-01-27 16:47:42 -0800

    fix

--- src/main/java/base/repository/BaseRepository.java
+++ src/main/java/base/repository/BaseRepository.java
@@ -3,10 +3,12 @@ package base.repository;
 import base.model.BaseEntity;
 
 import java.io.Serializable;
+import java.sql.SQLException;
 
 public interface BaseRepository<ID extends Serializable , TYPE extends BaseEntity<ID>> {
-    void save (TYPE type);
-    TYPE findById(ID id);
-    void update(TYPE type);
-    void delete(ID id);
+    void save (TYPE type) throws SQLException;
+    TYPE findById(ID id) throws SQLException;
+    void update(TYPE type) throws SQLException;
+    void delete(ID id) throws SQLException;
+
 }
