a6ccb81dd2096ba5eef79de9b8f0ee4830c29aa3
pc <poyatajhiz59@gmail.com>
2024-01-27 21:40:02 -0800

    creat and fill AdminRepositoryImpel class

---
+++ src/main/java/repository/AdminRepositoryImpel.java
@@ -0,0 +1,59 @@
+package repository;
+
+import base.repository.BaseRepositoryImpel;
+import models.Admins;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AdminRepositoryImpel extends BaseRepositoryImpel<Integer, Admins> implements AdminRepository {
+
+    public AdminRepositoryImpel(Connection connection) {
+        super(connection);
+    }
+
+    @Override
+    public String getTableName() {
+        return null;
+    }
+
+    @Override
+    public String getColumnsName() {
+        return null;
+    }
+
+    @Override
+    public String getCountOfQuestionMarkParams() {
+        return null;
+    }
+
+    @Override
+    public void fillParamForStatement(PreparedStatement preparedStatement, Admins admins, boolean isCountOnly) throws SQLException {
+
+    }
+
+    @Override
+    public Admins mapResultSetToEntity(ResultSet resultSet) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public String getUpdateQueryParams() {
+        return null;
+    }
+
+    @Override
+    public Admins login(String username, String password) throws SQLException {
+        String sql = "SELECT * FROM " + getTableName() + " WHERE admin_username = ? AND admin_password = ? ";
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next())
+                return mapResultSetToEntity(resultSet);
+        }
+        return null;
+    }
+    }
