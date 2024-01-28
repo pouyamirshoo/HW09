8ca94b5d2bfb8d1077e244c7841cb0b96b731bfb
pc <poyatajhiz59@gmail.com>
2024-01-28 09:41:22 -0800

    creat and fill BranchServiceImpel class

---
+++ src/main/java/service/BranchServiceImpel.java
@@ -0,0 +1,19 @@
+package service;
+
+import base.service.BaseServiceImpel;
+import models.Branch;
+import repository.BranchRepository;
+
+import java.sql.SQLException;
+
+
+public class BranchServiceImpel extends BaseServiceImpel<Integer, Branch, BranchRepository> implements BranchService{
+    public BranchServiceImpel(BranchRepository repository) {
+        super(repository);
+    }
+
+    @Override
+    public Branch[] showAllBranches() throws SQLException {
+        return new Branch[0];
+    }
+}
