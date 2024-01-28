8cfc04882fd4abe8801e912fa13cbb20496729df
pc <poyatajhiz59@gmail.com>
2024-01-27 19:40:20 -0800

    creat and fill UserService class

---
+++ src/main/java/service/UserServiceImpel.java
@@ -0,0 +1,19 @@
+package service;
+
+import base.service.BaseServiceImpel;
+import models.Users;
+import repository.UserRepository;
+
+import java.sql.SQLException;
+
+public class UserServiceImpel extends BaseServiceImpel<Integer, Users, UserRepository> implements UserService {
+
+    public UserServiceImpel(UserRepository repository) {
+        super(repository);
+    }
+
+    @Override
+    public Users logIn(String username,String password) throws SQLException {
+        return repository.logIn(username,password);
+    }
+}
