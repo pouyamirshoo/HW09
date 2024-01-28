50713e1e624b37394f05729d913bdb32ddc84be0
pc <poyatajhiz59@gmail.com>
2024-01-28 09:40:42 -0800

    creat and fill BranchRepositoryImpel class

---
+++ src/main/java/repository/BranchRepositoryImpel.java
@@ -0,0 +1,67 @@
+package repository;
+
+import base.repository.BaseRepositoryImpel;
+import models.Branch;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BranchRepositoryImpel extends BaseRepositoryImpel<Integer, Branch> implements BranchRepository{
+    public BranchRepositoryImpel(Connection connection) {
+        super(connection);
+    }
+
+    @Override
+    public String getTableName() {
+        return "branch";
+    }
+
+    @Override
+    public String getColumnsName() {
+        return "(branch_name)";
+    }
+
+    @Override
+    public String getCountOfQuestionMarkParams() {
+        return "(?)";
+    }
+
+    @Override
+    public void fillParamForStatement(PreparedStatement preparedStatement, Branch branch, boolean isCountOnly) throws SQLException {
+        preparedStatement.setString(1,branch.getBranchName());
+    }
+
+
+    @Override
+    public Branch mapResultSetToEntity(ResultSet resultSet) throws SQLException {
+        Branch branch = new Branch();
+
+        branch.setId(resultSet.getInt("branch_id"));
+        branch.setBranchName(resultSet.getString("branch_name"));
+        return branch;
+    }
+
+    @Override
+    public String getUpdateQueryParams() {
+        return "branch_name = ?";
+    }
+
+    @Override
+    public Branch[] showAllBranches() throws SQLException {
+        Branch [] branches = new Branch[numOfArray()];
+        int i = 0;
+
+        String sql = "SELECT FROM" + getTableName();
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                Branch branch = mapResultSetToEntity(resultSet);
+                branches[i] = branch;
+                i ++ ;
+            }
+        }
+        return branches;
+    }
+}
