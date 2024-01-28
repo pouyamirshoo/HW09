fa21743cd74b30903696ac05389fdab58f338d9f
pc <poyatajhiz59@gmail.com>
2024-01-28 12:37:49 -0800

    creat and fill SubBranchService class

---
+++ src/main/java/service/SubBranchService.java
@@ -0,0 +1,11 @@
+package service;
+
+import base.service.BaseService;
+import models.SubBranch;
+
+import java.sql.SQLException;
+
+public interface SubBranchService extends BaseService<Integer, SubBranch> {
+    SubBranch [] showAllSubBranches() throws SQLException;
+    int editBranchFk(int id,String name) throws SQLException;
+}
