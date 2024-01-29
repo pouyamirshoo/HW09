c4c328fa2d14f6158eaa299f90163df106fa4ec6
pc <poyatajhiz59@gmail.com>
2024-01-28 19:06:03 -0800

    creat and fill Products class

---
+++ src/main/java/models/Products.java
@@ -0,0 +1,25 @@
+package models;
+
+import base.model.BaseEntity;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Products extends BaseEntity<Integer> {
+    String productName;
+    float price;
+    int number;
+    SubBranch subBranch;
+
+    public Products(int productId, String productName, float price, int number, SubBranch subBranch) {
+        super(productId);
+        this.productName = productName;
+        this.price = price;
+        this.number = number;
+        this.subBranch = subBranch;
+    }
+}
