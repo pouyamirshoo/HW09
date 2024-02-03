86dfe46e86e8817f575c281a735d960ce50c0956
pc <poyatajhiz59@gmail.com>
2024-02-02 16:58:16 -0800

    creat and fill FactorRepository class

---
+++ src/main/java/repository/FactorRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import base.repository.BaseRepository;
+import models.Factor;
+
+import java.sql.SQLException;
+
+public interface FactorRepository extends BaseRepository<Integer, Factor> {
+    int [] numOfFactor(int id) throws SQLException;
+    int saveFactorInnerTable(int idF,int idP) throws SQLException;
+}
