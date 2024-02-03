29933dc4880c98f204d534bad1f6e2712128eba4
pc <poyatajhiz59@gmail.com>
2024-02-02 16:59:24 -0800

    creat and fill FactorServiceImpel class

---
+++ src/main/java/service/FactorServiceImpel.java
@@ -0,0 +1,24 @@
+package service;
+
+import base.repository.BaseRepositoryImpel;
+import base.service.BaseServiceImpel;
+import models.Factor;
+import repository.FactorRepository;
+
+import java.sql.SQLException;
+
+public class FactorServiceImpel extends BaseServiceImpel<Integer, Factor, FactorRepository> implements FactorService {
+    public FactorServiceImpel(FactorRepository repository) {
+        super(repository);
+    }
+
+    @Override
+    public int[] numOfFactor(int id) throws SQLException {
+        return repository.numOfFactor(id);
+    }
+
+    @Override
+    public int saveFactorInnerTable(int idF, int idP) throws SQLException {
+        return repository.saveFactorInnerTable(idF,idP);
+    }
+}
