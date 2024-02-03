cd73c64c0bffcb36b17331736cc55821a39a9482
pc <poyatajhiz59@gmail.com>
2024-02-02 16:59:12 -0800

    creat and fill FactorService class

---
+++ src/main/java/service/FactorService.java
@@ -0,0 +1,11 @@
+package service;
+
+import base.service.BaseService;
+import models.Factor;
+
+import java.sql.SQLException;
+
+public interface FactorService  extends BaseService<Integer, Factor> {
+    int [] numOfFactor(int id) throws SQLException;
+    int saveFactorInnerTable(int idF,int idP) throws SQLException;
+}
